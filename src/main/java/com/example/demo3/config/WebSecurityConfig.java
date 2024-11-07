79d0091a1ad5cfc8f4de91cd5651c49b727ac52c
akihikoisoe <180363344+akiso999@users.noreply.github.com>
2024-11-07 19:55:11 +0900

    修正

--- src/main/java/com/example/demo3/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo3/config/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.demo3.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,9 +11,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.example.demo3.service.AccountUserDetailsService;
 
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration // 設定を行うクラスであることを指定
+@EnableWebSecurity // Spring Securityを利用することを指定
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private AccountUserDetailsService userDetailsService;
@@ -34,8 +32,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// 認可の設定
-		http.exceptionHandling().accessDeniedPage("/accessDeniedPage").and().authorizeRequests()
-				.antMatchers("/loginForm").permitAll() // loginFormは、全ユーザからのアクセスを許可
+
+		http.authorizeRequests().antMatchers("/loginForm").permitAll() // loginFormは、全ユーザからのアクセスを許可
 				.anyRequest().authenticated(); // loginForm以外は、認証を求める
 
 		// ログイン設定
@@ -44,7 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.loginProcessingUrl("/authenticate") // フォーム認証処理のパス
 				.usernameParameter("userName") // ユーザ名のリクエストパラメータ名
 				.passwordParameter("password") // パスワードのリクエストパラメータ名
-				.defaultSuccessUrl("/posts") // ※ ここを変更
+				.defaultSuccessUrl("/home") // 認証成功時に遷移するデフォルトのパス
 				.failureUrl("/loginForm?error=true"); // 認証失敗時に遷移するパス
 
 		// ログアウト設定
